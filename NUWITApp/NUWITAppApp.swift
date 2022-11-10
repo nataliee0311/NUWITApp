8a560bd6f68a9666c92eba815c66d07dd8d05a2a
Natalie Laliberte <natalie.laliberte@Natalie-Laliberte.local>
2022-11-10 15:58:18 -0500

    started repo

---
+++ NUWITApp/NUWITAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NUWITAppApp.swift
+//  NUWITApp
+//
+//  Created by Natalie Laliberte on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct NUWITAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
